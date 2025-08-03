eeb7d5089b816a473423cfdd40b86dbb2a9e4190
Chanho Kim <chanhokim9848@gmail.com>
2025-08-03 17:01:58 +0900

    google authentication fix - AUG/03/25

--- src/auth.ts
+++ src/auth.ts
@@ -43,7 +43,7 @@ interface DatabaseUserAttributes {
 // for google authentication
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
-  process.env.GOOGLE_CLIENT_SECERT!,
+  process.env.GOOGLE_CLIENT_SECRET!,
   `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`,
 )
 
